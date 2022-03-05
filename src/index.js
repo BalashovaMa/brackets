18114c6d5c956b36972a4698873ebf262614835e
Balashova Maryna <balashova.m.nd@gmail.com>
2022-03-05 12:57:45 +0200

    reat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '{', '[', '|'];
+  const bracketPair = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['|']: '|',
+  }
+  
+  if (str.length % 2 !== 0) return false;
+  if (str.length == 0) return true;
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length == 0) {
+        return false;
+      }
+      let topElement = stack[stack.length - 1];
+      if (bracketPair[currentSymbol] == topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;// your solution
 }
